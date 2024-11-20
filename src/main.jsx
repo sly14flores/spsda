939377ad42eee08699abec32cd7383a80fc4bd5a
Sly Flores <sly@christian.com.ph>
2024-11-20 11:42:37 +0800

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,37 @@ import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+// const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: true,
+      refetchOnReconnect: true,
+      retry: false,
+      // staleTime: twentyFourHoursInMs,
+    },
+  },
+})
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
